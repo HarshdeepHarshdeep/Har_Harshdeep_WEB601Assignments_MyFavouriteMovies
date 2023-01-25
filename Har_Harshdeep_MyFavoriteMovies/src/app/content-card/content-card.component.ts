6f1ebca83703e6addaaaab349149f597ffb43fcd
Harshdeep <w0788912@myscc.ca>
2023-01-25 00:17:31 -0500

    update img link

--- Har_Harshdeep_MyFavoriteMovies/src/app/content-card/content-card.component.ts
+++ Har_Harshdeep_MyFavoriteMovies/src/app/content-card/content-card.component.ts
@@ -17,21 +17,21 @@ export class ContentCardComponent {
       title: "The Avengers",
       description: "Marvel's The Avengers or simply The Avengers, is a 2012 American superhero film based on the Marvel Comics superhero team of the same name.",
       creator: "Marvel",
-      imageUrl: "https://en.wikipedia.org/wiki/File:The_Avengers_(2012_film)_poster.jpg",
+      imageUrl: "https://upload.wikimedia.org/wikipedia/en/8/8a/The_Avengers_%282012_film%29_poster.jpg",
       type: "Action"
     });
     this.contentList.add({id: 2,
       title: "Avengers: Age of Ultron",
       description: "Avengers: Age of Ultron is a 2015 American superhero film based on the Marvel Comics superhero team the Avengers.",
       creator: "Marvel",
-      imageUrl: "https://en.wikipedia.org/wiki/File:Avengers_Age_of_Ultron_poster.jpg",
+      imageUrl: "https://upload.wikimedia.org/wikipedia/en/f/ff/Avengers_Age_of_Ultron_poster.jpg",
       type: "Action"
     });
     this.contentList.add({id: 3,
       title: "Avengers: Infinity War",
       description: "Avengers: Infinity War is a 2018 American superhero film based on the Marvel Comics superhero team the Avengers.",
       creator: "Marvel",
-      imageUrl: "https://en.wikipedia.org/wiki/File:Avengers_Infinity_War_poster.jpg",
+      imageUrl: "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg",
       type: "Action"
     });
   }
