6b3122f23e7149ba8ca8f4213f1ca16a1e45c6d9
Harshdeep <w0788912@myscc.ca>
2023-02-02 18:38:52 -0500

    added slice method

--- Har_Harshdeep_MyFavoriteMovies/src/app/content-card/content-card.component.ts
+++ Har_Harshdeep_MyFavoriteMovies/src/app/content-card/content-card.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Content } from '../helper-files/content-interface';
-import { ContentList } from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
@@ -8,31 +6,5 @@ import { ContentList } from '../helper-files/content-list';
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent {
-  contentList: ContentList;
-
-  constructor(){
-    this.contentList = new ContentList();
-
-    this.contentList.add({id: 1,
-      title: "The Avengers",
-      description: "Marvel's The Avengers or simply The Avengers, is a 2012 American superhero film based on the Marvel Comics superhero team of the same name.",
-      creator: "Marvel",
-      imageUrl: "https://upload.wikimedia.org/wikipedia/en/8/8a/The_Avengers_%282012_film%29_poster.jpg",
-      type: "Action"
-    });
-    this.contentList.add({id: 2,
-      title: "Avengers: Age of Ultron",
-      description: "Avengers: Age of Ultron is a 2015 American superhero film based on the Marvel Comics superhero team the Avengers.",
-      creator: "Marvel",
-      imageUrl: "https://upload.wikimedia.org/wikipedia/en/f/ff/Avengers_Age_of_Ultron_poster.jpg",
-      type: "Action"
-    });
-    this.contentList.add({id: 3,
-      title: "Avengers: Infinity War",
-      description: "Avengers: Infinity War is a 2018 American superhero film based on the Marvel Comics superhero team the Avengers.",
-      creator: "Marvel",
-      imageUrl: "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg",
-      type: "Action"
-    });
-  }
+ 
 }
--- Har_Harshdeep_MyFavoriteMovies/src/app/content-list/content-list.component.ts
+++ Har_Harshdeep_MyFavoriteMovies/src/app/content-list/content-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Content } from "../helper-files/content-interface";
 
 @Component({
   selector: 'app-content-list',
@@ -6,5 +7,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-list.component.scss']
 })
 export class ContentListComponent {
+  contents: Content[];
 
+    constructor(){
+        this.contents = [
+          {
+            id: 1,
+            title: "The Avengers",
+            description: "Marvel's The Avengers or simply The Avengers, is a 2012 American superhero film based on the Marvel Comics superhero team of the same name.",
+            creator: "Marvel",
+            imageUrl: "https://upload.wikimedia.org/wikipedia/en/8/8a/The_Avengers_%282012_film%29_poster.jpg",
+            type: "Action"
+          },
+
+          {id: 2,
+            title: "Avengers: Age of Ultron",
+            description: "Avengers: Age of Ultron is a 2015 American superhero film based on the Marvel Comics superhero team the Avengers.",
+            creator: "Marvel",
+            imageUrl: "https://upload.wikimedia.org/wikipedia/en/f/ff/Avengers_Age_of_Ultron_poster.jpg",
+            type: "Action"
+          },
+
+          {id: 3,
+            title: "Avengers: Infinity War",
+            description: "Avengers: Infinity War is a 2018 American superhero film based on the Marvel Comics superhero team the Avengers.",
+            creator: "Marvel",
+            imageUrl: "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg",
+            type: "Action"
+          },
+
+          {id: 4,
+            title: "Thor",
+            description: "hor is a 2011 American superhero film based on the Marvel Comics character of the same name. Produced by Marvel Studios and distributed by Paramount Pictures,[a] it is the fourth film in the Marvel Cinematic Universe (MCU).",
+            creator: "Marvel",
+            imageUrl: "https://upload.wikimedia.org/wikipedia/en/9/95/Thor_%28film%29_poster.jpg",
+            type: "Action"
+          },
+
+          {id: 5,
+            title: "Iron Man",
+            description: "Iron Man is a superhero appearing in American comic books published by Marvel Comics. The character was co-created by writer and editor Stan Lee, developed by scripter Larry Lieber, and designed by artists Don Heck and Jack Kirby.",
+            creator: "Marvel",
+            imageUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Iron_Man_%28circa_2018%29.png/220px-Iron_Man_%28circa_2018%29.png",
+            type: "Action"
+          },
+
+          {id: 6,
+            title: "Spider-Man",
+            description: "Spider-Man is a superhero appearing in American comic books published by Marvel Comics. Created by writer-editor Stan Lee and artist Steve Ditko, he first appeared in the anthology comic book Amazing Fantasy #15 (August 1962) in the Silver Age of Comic Books.",
+            creator: "Marvel",
+            imageUrl: "https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png",
+            type: "Action"
+          }
+        ];
+    }
+
+    getContentDetails(index: number){
+      let content = this.contents[index];
+      return `<h2>${content.title}</h2>
+              <p>${content.description}</p>
+              <p>Creator: ${content.creator}</p>
+              <p>Type: ${content.type}</p>
+              <img src="${content.imageUrl}">`;
+    }
 }
